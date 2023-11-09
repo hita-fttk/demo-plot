61d5d8717e18e23e064202ea402af6555e2ff48b
hita-fttk <fttkmcc77@gmail.com>
2023-11-10 00:41:04 +0900

    plotするための初期ファイルたちを作成。

---
+++ demo-plot/resources/views/dashboard.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <title>Demo-plot</title>
+</head>
+<body>
+    <h1>デモです</h1>
+    <div class="bg-red-300 flex justify-center">aaa</div>
+    <a class="h-400px bg-orange-500" href="{{ route('start') }}">aaa</a>  
+</body>
+</html>
